bcc7dc61d5a7248b6a56ec6953073759684af392
Mayken <mayu-satu@hotmail.com>
2024-09-08 17:33:40 -0500

    Cambios home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React , {useContext, useEffect} from 'react';
+import React, { useEffect} from 'react';
 import {Link} from "react-router-dom";
 import loginImage from '../assets/images/ecupark.png'; 
 import { useUser } from "../hooks/useUser";
@@ -14,26 +14,28 @@ export const Home = () => {
     });
   }, []); 
 
-  
-    return (
-        <div className="container-fluid">
-          <div className="row align-items-center">
-            <div className="col-md-6">
-              <img src={loginImage}  className="img-fluid" alt="Login ECUPARK" />
-              <div>Aqui va una imagen</div>
-            </div>
-            <div className="col-md-6 border border-primary d-flex align-items-center justify-content-center" style={{height: '100%'}}>
-              <div className="d-grid gap-2">              
-                <h1>BIENVENIDO</h1>
-                <Link to="/login" className="btn btn-primary btn-lg mt-20">
-                    Inicio de Sesión
-                </Link>
-                <Link to="/registrar" className="btn btn-primary btn-lg mt-20">
-                    Registrarse
-                </Link>
-              </div>
+  return (
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        <div className="col-md-6 d-none d-md-flex align-items-center justify-content-center bg-light">
+          <img src={loginImage} className="img-fluid" alt="Login ECUPARK" />
+        </div>
+        
+        <div className="col-md-6 d-flex align-items-center justify-content-center bg-primary text-white">
+          <div className="text-center">
+            <h1 className="mb-4">BIENVENIDO A ECUPARK</h1>
+            <p className="lead mb-5">La mejor solución para gestionar tu parqueo</p>
+            <div className="d-grid gap-3">
+              <Link to="/login" className="btn btn-light btn-lg">
+                Inicio de Sesión
+              </Link>
+              <Link to="/registrar" className="btn btn-outline-light btn-lg">
+                Registrarse
+              </Link>
             </div>
           </div>
         </div>
-      );
+      </div>
+    </div>
+  );
 };
