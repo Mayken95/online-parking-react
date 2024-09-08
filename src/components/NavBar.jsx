37878e6c1cf69b2c9aa3a998fecd4ad9499e6720
Mayken <mayu-satu@hotmail.com>
2024-09-08 17:44:05 -0500

    NarBar comentarios

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,28 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { useUser } from '../hooks/useUser';
-// export const NavBar = () => {
-//   return (
-//     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-//       <div className="container-fluid">
-//         <div className="collapse navbar-collapse" id="navbarNav">
-//           <ul className="navbar-nav">
-//             <li className="nav-item">
-//               <a className="nav-link" href="#">Reservar</a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" href="#">Mis Reservas</a>
-//             </li>
-//             <li className="nav-item">
-//               <a className="nav-link" href="#">Salir</a>
-//             </li>
-//           </ul>
-          
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
 
 export  const NavBar = () => {
   const { user } = useUser(); 
